8fc8c9a2d77636288a23795000316cffb55c0ba1
Ron Salm <ron.salm.69@gmail.com>
2019-08-18 21:28:47 +0100

    Refactor: remove all cucumber dirs from the designpatterns

---
+++ source/core/designpatterns/builder/gtest/gtest-builder.cpp
@@ -0,0 +1,16 @@
+#include "Builder.h"
+
+#include "Generic.h"
+
+#include <gtest/gtest.h>
+
+TEST( Construct, Default )
+{
+
+}
+
+int main(int argc, char **argv) 
+{
+    ::testing::InitGoogleTest(&argc, argv); 
+    return RUN_ALL_TESTS();
+}
