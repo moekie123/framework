8fc2fd0fd65c96dc3ed12643dd5d30511a4b27a2
Ron Salm <ron.salm.69@gmail.com>
2019-08-25 10:56:45 +0100

    Refactor: Documentation improved, Mainpage is now in README.md and a file on it's own.

--- source/Framework.cpp
+++ source/Framework.cpp
@@ -9,18 +9,6 @@
 #include "hardware/drivers/chipset/Chipset.h"
 #include "hardware/drivers/device/Device.h"
 
-// TODO This should be moved to a documenation page
-/**
- * @mainpage  Documentation for the Autonomous Bar Tender (Robotic Arm)
- * 	This is the documentation for a framework to create and control an robotic arm. 
- *
- *	This doxygen generated webpages are describing the code base. 
- * 	Detailed design choices van be found on the <a href="https://moekiehome.wpcomstaging.com"> WordPress Pages </a> 
- *
- * @section Introduction
- *	This is de doxygen generated documentation for this Robotic Arm project
- */
-
 Framework::Framework( int argc, char *argv[] )
 {
 	Factory& factory = Singleton< Factory >::Instance();
