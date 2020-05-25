3c9104817a3b21c339db53dc3323e9224115d73d
Ron Salm <ron.salm.69@gmail.com>
2020-05-25 23:49:52 +0100

    DESC: Updatee Mosquitto Interface
    TYPE: BUGFIX/ NEW_FEATURE / REFACTOR / DOCUMENTATION

--- include/IMosquitto.h
+++ include/IMosquitto.h
@@ -2,11 +2,13 @@
 
 // Inheritance
 #include "Generic.h"
+#include "Visitor.h"
 
 /** 
  * @brief The Mosquitto Interface
  * @details Note that only Parameters are allowed to attach to the broker
  */
-class IMosquitto : public Generic
+class IMosquitto : public Generic,
+                   public MosquittoVisitor
 {
 };
