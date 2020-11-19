b0c13ed044356a26b412b53d580ecdb8dc3b11da
rsalm <rsalm>
2020-11-19 14:09:15 +0100

    BACKUP: Added spdlogger as sepeaerte cmake file

---
+++ external/cmake/spdlogger.cmake
@@ -0,0 +1,43 @@
+include(ExternalProject)
+
+# Instal the Google Test Framework
+set( EXTERNAL_PROJECT_NAME ext_spdlogger )
+
+# Configure build arguments
+set( EXTERNAL_CMAKE_ARGS -DSPDLOG_BUILD_SHARED=ON )
+list( APPEND EXTERNAL_CMAKE_ARGS -DSPDLOG_BUILD_TESTS=OFF )
+list( APPEND EXTERNAL_CMAKE_ARGS -DSPDLOG_BUILD_TESTS_HO=OFF )
+
+# Add cross compiler arguments to build arguments
+if( CROSS_COMPILING )
+	list( APPEND EXTERNAL_CMAKE_ARGS ${EXTERNAL_CROSS_COMPILE_ARGS} )
+endif()
+
+# Create external project
+ExternalProject_Add( ${EXTERNAL_PROJECT_NAME}
+	GIT_REPOSITORY https://github.com/gabime/spdlog.git
+	GIT_TAG "v1.6.0"
+	GIT_PROGRESS ON
+
+	UPDATE_DISCONNECTED EXTERNAL_UPDATE
+
+	DOWNLOAD_DIR    "${CMAKE_CURRENT_SOURCE_DIR}/${EXTERNAL_PROJECT_NAME}"
+	SOURCE_DIR      "${CMAKE_CURRENT_SOURCE_DIR}/${EXTERNAL_PROJECT_NAME}"
+	BINARY_DIR      "${CMAKE_CURRENT_SOURCE_DIR}/${EXTERNAL_PROJECT_NAME}"
+	INSTALL_DIR     "${CMAKE_CURRENT_SOURCE_DIR}/${EXTERNAL_PROJECT_NAME}"
+
+	TMP_DIR         "${EXTERNAL_TMP_DIR}/${EXTERNAL_PROJECT_NAME}"
+	STAMP_DIR       "${EXTERNAL_STAMP_DIR}/${EXTERNAL_PROJECT_NAME}"
+	LOG_DIR         "${EXTERNAL_LOG_DIR}/${EXTERNAL_PROJECT_NAME}"
+
+	CMAKE_ARGS 	${EXTERNAL_CMAKE_ARGS}
+ )
+
+# Refresh dynamic link libraries ( only on Raspberry Pi hardware )
+if( NOT CROSS_COMPILING )
+	ExternalProject_Add_Step( ${EXTERNAL_PROJECT_NAME} link
+		ALWAYS FALSE
+		DEPENDEES "install"
+		COMMENT "Link Dynamic Libraries"
+		COMMAND ldconfig )
+endif()
